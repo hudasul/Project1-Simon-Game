482dbeb869a2aa00938256d1ce30b3a7c2a14896
Bint Alhuda Sultan <bntalhdysltan@gmail.com>
2025-07-11 14:11:55 +0300

    created cheakPattern function

--- scripts/app.js
+++ scripts/app.js
@@ -1,56 +1,77 @@
 /*-------------------------------- Constants --------------------------------*/
-
-const ComputerPattern = []
-const playerPattern = []
+const ComputerPattern = [];
 
 /*---------------------------- Variables (state) ----------------------------*/
-let FlashedButtonID 
-let buttonsElem  
-let clickedButton
-let level = 0
+let FlashedButtonID;
+let buttonsElem;
+let clickedButton;
+let level = 1;
+let message;
+let playerPattern = [];
+let patternMatch;
 
 /*------------------------ Cached Element References ------------------------*/
-const message = document.querySelector("message")
+
 /*-------------------------------- Functions --------------------------------*/
-function init(){
-  buttonsElem = document.querySelectorAll(".button") 
-  flashButton()
-  buttonsElem.forEach(button => {
-    button.addEventListener("click", handleClick)
-  })
+function init() {
+  message = document.querySelector("#message");
+  buttonsElem = document.querySelectorAll(".button");
+
+  setTimeout(() => {
+        flashButton()
+      }, 2000);
+  
+
+  buttonsElem.forEach((button) => {
+    button.addEventListener("click", handleClick);
+  });
 }
 
-function createRandomNum(){
-  FlashedButtonID = Math.floor(Math.random() * 4)
-  return FlashedButtonID
+function createRandomNum() {
+  FlashedButtonID = Math.floor(Math.random() * 4);
+  return FlashedButtonID;
 }
 
-function flashButton(){
-  FlashedButtonID = createRandomNum()
-  buttonsElem.forEach(button => {
+function flashButton() {
+  FlashedButtonID = createRandomNum();
+  console.log(FlashedButtonID);
+  buttonsElem.forEach((button) => {
     if (Number(button.id) === FlashedButtonID) {
-      ComputerPattern.push(button.id)
-      console.log("computer = " + ComputerPattern)
-      button.style.backgroundColor = "grey"
+      ComputerPattern.push(button.id);
+      button.style.backgroundColor = "grey";
       setTimeout(() => {
-        button.style.backgroundColor = ""
-      }, 1000)
+        button.style.backgroundColor = "";
+      }, 1000);
     }
-  })
+  });
+}
+
+function checkPattern() {
+  console.log(ComputerPattern);
+  console.log(playerPattern);
+  for (let i = 0; i < playerPattern; i++) {
+    if (playerPattern[i] === Number(ComputerPattern[i])) {
+      message.textContent = "Level " + level;
+      playerPattern = [];
+      return (patternMatch = true);
+    } else {
+      message.textContent = `You lost at level ${level}!`;
+      return (patternMatch = false);
+    }
+  }
 }
 
 function handleClick(event) {
-  console.log(event.target.id)
-  clickedButton = event.target.id
-  playerPattern.push(clickedButton)
-  console.log("player " + playerPattern)
-  if(clickedButton === FlashedButtonID){
-    level++
-
+  clickedButton = event.target.id;
+  playerPattern.push(Number(clickedButton));
+  checkPattern();
+  console.log(patternMatch);
+  if (patternMatch === true) {
+    flashButton();
+  } else {
+    return;
   }
-  flashButton()
-  
 }
 
 /*----------------------------- Event Listeners -----------------------------*/
-document.addEventListener("DOMContentLoaded", init)
+document.addEventListener("DOMContentLoaded", init);
