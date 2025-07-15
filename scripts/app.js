d767657851853d34874c57c56f8fb1199a07a375
Bint Alhuda Sultan <bntalhdysltan@gmail.com>
2025-07-15 16:22:27 +0300

    modified help player functionality

--- scripts/app.js
+++ scripts/app.js
@@ -4,20 +4,18 @@ let buttonsElem;
 let startBtn;
 let quitBtn;
 let message;
-
-// an array the has the ids of the player clicked buttons
-let playerPattern = [];
-
-// an array the has th ids of the computer clicked buttons
-// buttens chossen randomly based on a random number
-const ComputerPattern = [];
-
-// a lampimage thet offers help to the player once it is clicked
-// help the player one time only throughout the game
 let lamp;
 
+// used for hel player function
+let helpMode = false;
+
+// an array that has the ids of the player clicked buttons
+let playerPattern = [];
+
+// an array that has the ids of the computer clicked buttons
+const ComputerPattern = [];
+
 function init() {
-  // cached elements references
   quitBtn = document.querySelector("#quit");
   startBtn = document.querySelector("#startBtn");
   message = document.querySelector("#message");
@@ -25,30 +23,29 @@ function init() {
   lamp = document.querySelector("#help-lamp");
 
   lamp.classList.remove("hidden");
+  helpMode = false;
+
   startBtn.addEventListener("click", startGame);
   startBtn.style.backgroundColor = "black";
 
   quitBtn.addEventListener("click", function () {
     init();
     quitBtn.style.backgroundColor = "grey";
-    message.textContent = `You have quited the game at level ${level}`;
+    message.textContent = `You quited the game at level ${level}`;
     message.style.color = "white";
     startBtn.textContent = "Play Again";
     lamp.classList.add("hidden");
     startBtn.disabled = false;
   });
 
-  // player not allowed to click the colors unliss he click start button
-  // also, player cant do that when he quit the game
   disableButtonClick();
 }
-
-//functions
 
 function startGame() {
   lamp.classList.remove("hidden");
   startBtn.style.backgroundColor = "grey";
   quitBtn.style.backgroundColor = "black";
+
   buttonsElem.forEach(function (button) {
     button.addEventListener("click", handleClick);
   });
@@ -58,13 +55,13 @@ function startGame() {
   ComputerPattern.length = 0;
   playerPattern.length = 0;
   level = 1;
+  helpMode = false;
 
   message.textContent = `Level ${level}`;
   message.style.color = "white";
   startBtn.disabled = true;
-  setTimeout(function () {
-    flashButton();
-  }, 1000);
+
+  setTimeout(flashButton, 1000);
 }
 
 function creatRandomNumber() {
@@ -75,7 +72,6 @@ function creatRandomNumber() {
 function flashButton() {
   id = creatRandomNumber();
   ComputerPattern.push(id);
-  // choose sound from sounds array based on the id/index of the button
   makeSound(id);
 
   buttonsElem.forEach(function (button) {
@@ -106,8 +102,6 @@ function changeColor(button) {
 function checkPattern() {
   for (let i = 0; i < playerPattern.length; i++) {
     if (playerPattern[i] !== ComputerPattern[i]) {
-      // player not allowed to click on the colors if he lost
-      //player has to click "play again" to be able to click/play again
       disableButtonClick();
       return false;
     }
@@ -117,36 +111,22 @@ function checkPattern() {
 
 function helpPlayer() {
   lamp.classList.add("hidden");
-  let index = playerPattern.length;
-  message.style.color = "white";
+  helpMode = true;
+  updateHelpMessage();
+}
 
-  if (ComputerPattern[index] === 0) {
-    message.textContent = "Click Red";
-    setTimeout(function () {
-      message.textContent = `Level ${level}`;
-      message.style.color = "green";
-    }, 1000);
+function updateHelpMessage() {
+  if (helpMode === false){
+    return
+  } 
 
-  } else if (ComputerPattern[index] === 1) {
-    message.textContent = "Click Green";
-    setTimeout(function () {
-      message.textContent = `Level ${level}`;
-      message.style.color = "green";
-    }, 1000);
+  const colors= ["Red", "Green", "Blue", "Yellow"];
+  const index = playerPattern.length;
 
-  } else if (ComputerPattern[index] === 2) {
-    message.textContent = "Click Blue";
-    setTimeout(function () {
-      message.textContent = `Level ${level}`;
-      message.style.color = "green";
-    }, 1000);
-
-  } else {
-    message.textContent = "Click Yellow";
-    setTimeout(function () {
-      message.textContent = `Level ${level}`;
-      message.style.color = "green";
-    }, 1000);
+  if (index < ComputerPattern.length) {
+    const nextColor = colors[ComputerPattern[index]];
+    message.textContent = `Click ${nextColor}`;
+    message.style.color = "white";
   }
 }
 
@@ -155,9 +135,8 @@ function flashNextButton() {
   message.textContent = `Level ${level}`;
   message.style.color = "green";
   playerPattern = [];
-  setTimeout(function () {
-    flashButton();
-  }, 1000);
+
+  setTimeout(flashButton, 1000);
 }
 
 function disableButtonClick() {
@@ -176,23 +155,22 @@ function handleClick(event) {
     message.textContent = `You lost at level ${level}!`;
     message.style.color = "red";
     startBtn.style.backgroundColor = "black";
-    const gameOver = new Audio("/assets/wrong.mp3");
-    gameOver.play();
-
+    new Audio("/assets/wrong.mp3").play();
     lamp.classList.add("hidden");
     startBtn.disabled = false;
     startBtn.textContent = "Play Again";
+    return;
   }
 
-  if (
-    checkPattern() === true &&
-    playerPattern.length === ComputerPattern.length
-  ) {
-    setTimeout(function () {
-      flashNextButton();
-    }, 1000);
+  if (helpMode === true) {
+    updateHelpMessage();
+  }
+
+  if (checkPattern() === true && playerPattern.length === ComputerPattern.length) {
+    helpMode = false;
+    setTimeout(flashNextButton, 1000);
   }
 }
 
-// event listen, calling the "init" function as the page load
-document.addEventListener("DOMContentLoaded", init);
+// Load game when page opens
+document.addEventListener("DOMContentLoaded", init)
