b474583fb10eee678a6e071b4210a631f5692509
Bint Alhuda Sultan <bntalhdysltan@gmail.com>
2025-07-16 11:51:33 +0300

    just cleand the code, make it look better/readable and removed simecolumns

--- scripts/app.js
+++ scripts/app.js
@@ -1,87 +1,87 @@
-let id;
-let level = 1;
-let buttonsElem;
-let startBtn;
-let quitBtn;
-let message;
-let lamp;
+let id
+let level = 1
+let buttonsElem
+let startBtn
+let quitBtn
+let message
+let lamp
 
 // used for hel player function
-let helpMode = false;
+let helpMode = false
 
 // an array that has the ids of the player clicked buttons
-let playerPattern = [];
+let playerPattern = []
 
 // an array that has the ids of the computer clicked buttons
-const ComputerPattern = [];
+const ComputerPattern = []
 
 function init() {
-  quitBtn = document.querySelector("#quit");
-  startBtn = document.querySelector("#startBtn");
-  message = document.querySelector("#message");
-  buttonsElem = document.querySelectorAll(".button");
-  lamp = document.querySelector("#help-lamp");
+  quitBtn = document.querySelector("#quit")
+  startBtn = document.querySelector("#startBtn")
+  message = document.querySelector("#message")
+  buttonsElem = document.querySelectorAll(".button")
+  lamp = document.querySelector("#help-lamp")
 
-  lamp.classList.remove("hidden");
+  lamp.classList.remove("hidden")
   message.style.marginLeft = "5px"
-  helpMode = false;
+  helpMode = false
 
-  startBtn.addEventListener("click", startGame);
-  startBtn.style.backgroundColor = "black";
+  startBtn.addEventListener("click", startGame)
+  startBtn.style.backgroundColor = "black"
 
   quitBtn.addEventListener("click", function () {
-    init();
-    quitBtn.style.backgroundColor = "grey";
-    message.textContent = `You quited the game at level ${level}`;
-    message.style.color = "white";
-    startBtn.textContent = "Play Again";
-    lamp.classList.add("hidden");
+    init()
+    quitBtn.style.backgroundColor = "grey"
+    message.textContent = `You quited the game at level ${level}`
+    message.style.color = "white"
+    startBtn.textContent = "Play Again"
+    lamp.classList.add("hidden")
     message.style.marginLeft = "-30px"
-    startBtn.disabled = false;
-  });
+    startBtn.disabled = false
+  })
 
-  disableButtonClick();
+  disableButtonClick()
 }
 
 function startGame() {
-  lamp.classList.remove("hidden");
+  lamp.classList.remove("hidden")
   message.style.marginLeft = "5px"
-  startBtn.style.backgroundColor = "grey";
-  quitBtn.style.backgroundColor = "black";
+  startBtn.style.backgroundColor = "grey"
+  quitBtn.style.backgroundColor = "black"
 
   buttonsElem.forEach(function (button) {
-    button.addEventListener("click", handleClick);
-  });
+    button.addEventListener("click", handleClick)
+  })
 
-  lamp.addEventListener("click", helpPlayer);
+  lamp.addEventListener("click", helpPlayer)
 
-  ComputerPattern.length = 0;
-  playerPattern.length = 0;
-  level = 1;
-  helpMode = false;
+  ComputerPattern.length = 0
+  playerPattern.length = 0
+  level = 1
+  helpMode = false
 
-  message.textContent = `Level ${level}`;
-  message.style.color = "white";
-  startBtn.disabled = true;
+  message.textContent = `Level ${level}`
+  message.style.color = "white"
+  startBtn.disabled = true
 
-  setTimeout(flashButton, 1000);
+  setTimeout(flashButton, 1000)
 }
 
 function creatRandomNumber() {
-  id = Math.floor(Math.random() * 4);
-  return id;
+  id = Math.floor(Math.random() * 4)
+  return id
 }
 
 function flashButton() {
-  id = creatRandomNumber();
-  ComputerPattern.push(id);
-  makeSound(id);
+  id = creatRandomNumber()
+  ComputerPattern.push(id)
+  makeSound(id)
 
   buttonsElem.forEach(function (button) {
     if (Number(button.id) === id) {
-      changeColor(button);
+      changeColor(button)
     }
-  });
+  })
 }
 
 function makeSound(id) {
@@ -90,95 +90,95 @@ function makeSound(id) {
     "/assets/green.mp3",
     "/assets/blue.mp3",
     "/assets/yellow.mp3",
-  ];
-  const audio = new Audio(sounds[id]);
-  audio.play();
+  ]
+  const audio = new Audio(sounds[id])
+  audio.play()
 }
 
 function changeColor(button) {
-  button.style.backgroundColor = "grey";
+  button.style.backgroundColor = "grey"
   setTimeout(function () {
-    button.style.backgroundColor = "";
-  }, 500);
+    button.style.backgroundColor = ""
+  }, 500)
 }
 
 function checkPattern() {
   for (let i = 0; i < playerPattern.length; i++) {
     if (playerPattern[i] !== ComputerPattern[i]) {
-      disableButtonClick();
-      return false;
+      disableButtonClick()
+      return false
     }
   }
-  return true;
+  return true
 }
 
 function helpPlayer() {
-  lamp.classList.add("hidden");
+  lamp.classList.add("hidden")
   message.style.marginLeft = "-25px"
-  helpMode = true;
-  updateHelpMessage();
+  helpMode = true
+  updateHelpMessage()
 }
 
 function updateHelpMessage() {
   if (helpMode === false) {
-    return;
+    return
   }
 
-  const colors = ["Red", "Green", "Blue", "Yellow"];
-  const index = playerPattern.length;
+  const colors = ["Red", "Green", "Blue", "Yellow"]
+  const index = playerPattern.length
 
   if (index < ComputerPattern.length) {
-    const nextColor = colors[ComputerPattern[index]];
-    message.textContent = `Click ${nextColor}`;
-    message.style.color = "white";
+    const nextColor = colors[ComputerPattern[index]]
+    message.textContent = `Click ${nextColor}`
+    message.style.color = "white"
   }
 }
 
 function flashNextButton() {
-  level++;
-  message.textContent = `Level ${level}`;
-  message.style.color = "green";
-  playerPattern = [];
+  level++
+  message.textContent = `Level ${level}`
+  message.style.color = "green"
+  playerPattern = []
 
-  setTimeout(flashButton, 1000);
+  setTimeout(flashButton, 1000)
 }
 
 function disableButtonClick() {
   buttonsElem.forEach(function (button) {
-    button.removeEventListener("click", handleClick);
-  });
+    button.removeEventListener("click", handleClick)
+  })
 }
 
 function handleClick(event) {
-  const clickedID = Number(event.target.id);
-  makeSound(clickedID);
-  changeColor(event.target);
-  playerPattern.push(clickedID);
+  const clickedID = Number(event.target.id)
+  makeSound(clickedID)
+  changeColor(event.target)
+  playerPattern.push(clickedID)
 
   if (checkPattern() === false) {
-    message.textContent = `You lost at level ${level}!`;
-    message.style.color = "red";
-    startBtn.style.backgroundColor = "black";
-    new Audio("/assets/wrong.mp3").play();
-    lamp.classList.add("hidden");
+    message.textContent = `You lost at level ${level}!`
+    message.style.color = "red"
+    startBtn.style.backgroundColor = "black"
+    new Audio("/assets/wrong.mp3").play()
+    lamp.classList.add("hidden")
     message.style.marginLeft = "-37px"
-    startBtn.disabled = false;
-    startBtn.textContent = "Play Again";
-    return;
+    startBtn.disabled = false
+    startBtn.textContent = "Play Again"
+    return
   }
 
   if (helpMode === true) {
-    updateHelpMessage();
+    updateHelpMessage()
   }
 
   if (
     checkPattern() === true &&
     playerPattern.length === ComputerPattern.length
   ) {
-    helpMode = false;
-    setTimeout(flashNextButton, 1000);
+    helpMode = false
+    setTimeout(flashNextButton, 1000)
   }
 }
 
 // Load game when page opens
-document.addEventListener("DOMContentLoaded", init);
+document.addEventListener("DOMContentLoaded", init)
